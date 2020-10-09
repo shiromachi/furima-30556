8c342ace68b0910058af8ff9d419d4d9233e294a
shiromachi <mustard.dreieck@gmail.com>
2020-10-09 12:40:05 +0900

    新規登録機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
-  validates :birthday, presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "is invalid. Input full-width characters." }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "is invalid. Input full-width characters." }
+    validates :last_name_kana, format: { with: /\A([ァ-ン]|ー)+\z/, message: "is invalid. Input full-width katakana characters." }
+    validates :first_name_kana, format: { with: /\A([ァ-ン]|ー)+\z/, message: "is invalid. Input full-width katakana characters." }
+    validates :birthday
+  end
 
   has_many :items
 end
