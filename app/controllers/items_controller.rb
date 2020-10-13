0910d4228c2e050d685c4868ef2e74ce30694a86
shiromachi <mustard.dreieck@gmail.com>
2020-10-13 14:55:55 +0900

    商品一覧表示機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :move_to_new, only: :new
 
   def index
-    @item = Item.all
+    @items = Item.all
   end
 
   def new
@@ -16,6 +16,10 @@ class ItemsController < ApplicationController
     else
       render :new
     end
+  end
+
+  def show
+    @item = Item.find(params[:id])
   end
 
   private
