936430317aaceff1668c313d73e336f65c2b078b
shiromachi <mustard.dreieck@gmail.com>
2020-10-22 21:01:52 +0900

    SNSログイン機能の追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
 
   NAME_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/.freeze
   NAME_REGEX_KANA = /\A([ァ-ン]|ー)+\z/.freeze
@@ -21,4 +21,19 @@ class User < ApplicationRecord
   has_many :items
   has_many :managements
   has_many :comments
+  has_many :sns_credentials
+  
+  def self.from_omniauth(auth)
+    sns = SnsCredential.where(provider: auth.provider, uid: auth.uid).first_or_create
+    user = User.where(email: auth.info.email).first_or_initialize(
+      nickname: auth.info.name,
+        email: auth.info.email
+    )
+
+    if user.persisted?
+      sns.user = user
+      sns.save
+    end
+    { user: user, sns: sns }
+  end
 end
